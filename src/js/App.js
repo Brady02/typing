e649beba3a1ee530b6e2f3ff48f1fa1fc78ae540
Brady Warren <brady.warren@outlook.com>
2025-01-16 19:15:11 -0400

    page and tailwind setup

---
+++ src/js/App.js
@@ -0,0 +1,21 @@
+import '../css/App.css';
+import TypingWindow from './typingWindow';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+
+      <TypingWindow>
+      </TypingWindow>
+
+      <footer>
+        
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/js/typingWindow.js
@@ -0,0 +1,21 @@
+import { generate } from "random-words";
+
+const TypingWindow = () => {
+
+
+
+    return (
+        <div>
+            <div class="flex space-x-4">
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+            </div>
+
+
+        </div>
+        
+    );
+}
+
+export default TypingWindow;
